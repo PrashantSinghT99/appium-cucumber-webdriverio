27db191eb1696bbf6f7f0225da3afe5d9f564f53
Prashant Singh <prashantsinghtariyal@gmail.com>
2024-06-22 11:08:50 +0530

    implemented all testcases added reusability tags yet to implement [pom]

--- features/login.feature
+++ features/login.feature
@@ -3,10 +3,43 @@ Feature: Login to SauceLabs mobile application
   Background:
     Given I am on the login page
 
-  Scenario Outline: As a user, I can log into with valid credentials
-    When I login with <username> and <password>
+  @valid
+  Scenario Outline: Valid username and password
+    When I enter the username <username>
+    Then I enter the password <password>
+    Then I click the login button
     Then I validate product screen visible
 
     Examples:
       | username      | password     |
       | standard_user | secret_sauce |
+
+  @invalid
+  Scenario Outline: Invalid username and password
+    When I enter the username <username>
+    Then I enter the password <password>
+    Then I click the login button
+    Then I should see the error message <message>
+
+    Examples:
+      | username      | password     | message                                                      |
+      | alice@example.com | 10203040 | Username and password do not match any user in this service. |
+
+  @noCredentials
+  Scenario Outline: Validate no username and no password
+    When I click the login button
+    Then I should see the error message <message>
+
+    Examples:
+      | message              |
+      | Username is required |
+
+  @usernameNoPassword
+  Scenario Outline: Validate enter username but no password
+    When I enter the username <username>
+    Then I click the login button
+    Then I should see the error message <message>
+
+    Examples:
+      | username        | message              |
+      | bob@example.com | Password is required |
--- features/step-definitions/loginstepdefs.js
+++ features/step-definitions/loginstepdefs.js
@@ -5,23 +5,38 @@ Given(/^I am on the login page$/, async function () {
     console.log('Application launched');
 });
 
-When(/^I login with (.*) and (.*)$/, async function (username, password) {
-    // Code to enter username and password and click login button
-    // console.log(`${username} ${password}`);
-
+When(/^I enter the username (.*)$/, async function (username) {
     const usernameField = await $('~test-Username');
     await usernameField.setValue(username);
+});
 
+
+Then(/^I enter the password (.*)$/, async function (password) {
     const passwordField = await $('~test-Password');
     await passwordField.setValue(password);
+});
 
+Then(/^I click the login button$/, async function () {
     const loginButton = await $('~test-LOGIN');
     await loginButton.click();
 });
 
+
 Then(/^I validate product screen visible$/, async function () {
-    // Code to validate product screen visibility
-    // console.log('Validate product visible on screen stepdef');
     const actual = await $("//android.widget.TextView[@text='PRODUCTS']");
     await expect(actual).toBeExisting();
+});
+
+
+Then(/^I should see the error message (.*)$/, async function (message) {
+    // Locate the error message element using XPath
+    //const errorMessageElement = await $('//android.widget.TextView[@text="Username and password do not match any user in this service."]');
+    const errorMessageElement = await $(`//android.widget.TextView[@text="${message}"]`);
+
+    // Get the text content of the error message element
+    const errorMessageText = await errorMessageElement.getText();
+    console.log('Error message:', errorMessageText);
+
+    // Assert that the error message text is as expected
+    expect(errorMessageText).toBe(message);
 });
